5b14cd75fd1f3256802fedf2dbd148789f021fa8
Jacky Tea <teajacks@outlook.com>
2020-04-14 16:02:01 -0400

    add score colors

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -7,6 +7,9 @@
 
 //global counter
 let counter = 0;
+let red = 255;
+let green = 255;
+let blue = 255;
 
 //clear starting prompt
 function removeStartMessage() {
@@ -16,19 +19,49 @@ function removeStartMessage() {
 
 //keep score
 function keepScore(counter) {
+    console.log(red, green, blue)
     let points = document.getElementById('points');
     let number = document.createTextNode(counter + 'x');
     points.innerHTML = "";
+    if (green > 234 || blue > 0) {
+        if (green > 234) {
+            green -= 10;
+        }
+        if (blue > 0) {
+            blue -= 10;
+        }
+        points.style.color = "rgb(255," + green + "," + blue + ")";
+    } else if (green > 0 && blue <= 0) {
+        if (green > 0) {
+            green -= 10;
+        }
+        points.style.color = "rgb(255," + green + "," + blue + ")";
+    } else if (green <= 0 && blue <= 0 && red > 0) {
+        if (red > 0) {
+            red -= 10;
+        }
+        points.style.color = "rgb(" + red + "," + green + "," + blue + ")";
+    } else if (red <= 0 && green <= 0 && blue <= 0) {
+        number = document.createTextNode("SUPER! " + counter + 'x');
+        points.style.color = "white";
+        points.style.animation = "rainbow infinite 3.5s";
+    }
     points.appendChild(number);
 }
 
 //reset game
 function resetGame() {
     counter = 0;
+    red = 255;
+    green = 255;
+    blue = 255;
     keepScore(0);
     let prompt = document.getElementById('start-dialog');
     let h1 = document.createElement('h1');
     let msg = document.createTextNode('Game reset!');
+    let points = document.getElementById('points');
+    points.style.animation = "none";
+    points.style.color = "white";
     prompt.innerHTML = "";
     h1.appendChild(msg);
     prompt.appendChild(h1);
